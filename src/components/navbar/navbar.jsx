50746b9d3433997455480f2ae9a7e3dc19853af3
your name here <echen831@gmail.com>
2021-01-11 12:48:44 -0800

    update navbar

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,17 +1,28 @@
 import React from 'react';
 import { LinkButton } from '../portfolio/buttons'
+import { Link } from 'react-router-dom';
 import './navbar.css';
 
 export const Navbar = ({ page }) => {
 
-    const button = page !== 'splash' ? 
-                    <LinkButton currIdx={1} link={''} text={'Home'} /> :
-                    <LinkButton currIdx={1} link={'portfolio'} text={'Get Started'} />
-    return(
-        <div className='navbar-header'>
-            <h1>Easy Finance Advisor</h1>
-            {button}
+    // const button = page !== 'splash' ? 
+    //                 <LinkButton currIdx={1} link={''} text={'Home'} /> :
+    //                 <LinkButton currIdx={1} link={'portfolio'} text={'Get Started'} />
 
+    const link = page !== 'splash' ? 
+                <Link to=''>Home</Link> :
+                <Link to='/portfolio'>Get Started</Link>
+    return(
+        <div className='navbar'>
+            <div className='navbar-container flex'>              
+                <h1 className='logo'>Easy Finance Advisor</h1>
+                <nav>
+                    <ul>
+                        <li>{link}</li>
+                    </ul>
+                </nav>
+            </div>
+            
         </div>
     )
 }
